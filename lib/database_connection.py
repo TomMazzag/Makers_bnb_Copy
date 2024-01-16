a20bc38806dd6d2c0f6d9eb6c8cbf60f1eb2f8a6
TomMazzag <itstomazz@gmail.com>
2024-01-16 17:22:17 +0000

    Testing deployment

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -22,7 +22,8 @@ class DatabaseConnection:
     def connect(self):
         try:
             self.connection = psycopg.connect(
-                f"postgresql://localhost/{self._database_name()}", row_factory=dict_row
+                f"postgresql://{os.environ.get('POSTGRES_DB')}@ep-broken-sunset-89720119.eu-central-1.aws.neon.tech/{self._database_name()}?sslmode=require",
+                row_factory=dict_row
             )
         except psycopg.OperationalError:
             raise Exception(
